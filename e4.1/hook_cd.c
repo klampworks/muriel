dbc73e9480169d45ab7e6b14d582625cecd24db0
yukipyon <wolfvbear@gmail.com>
2016-03-27 18:16:38 +0000

    This char device does not exist. There is no point creating a new one because then we control the handler functions from the start. Lets try /dev/null.

--- e4.1/hook_cd.c
+++ e4.1/hook_cd.c
@@ -28,10 +28,11 @@ static int load(struct module *module, int cmd, void *args)
 
     switch (cmd) {
         case MOD_LOAD:
+            
             mtx_lock(&devmtx);
 
             TAILQ_FOREACH(cdp, &cdevp_list, cdp_list) {
-                if (strcmp(cdp->cdp_c.si_name, "cd_example") == 0) {
+                if (strcmp(cdp->cdp_c.si_name, "null") == 0) {
                     read = cdp->cdp_c.si_devsw->d_read;
                     cdp->cdp_c.si_devsw->d_read = read_hook;
                     break;
@@ -45,7 +46,7 @@ static int load(struct module *module, int cmd, void *args)
             mtx_lock(&devmtx);
 
             TAILQ_FOREACH(cdp, &cdevp_list, cdp_list) {
-                if (strcmp(cdp->cdp_c.si_name, "cd_example") == 0) {
+                if (strcmp(cdp->cdp_c.si_name, "null") == 0) {
                     cdp->cdp_c.si_devsw->d_read = read;
                     break;
                 }
